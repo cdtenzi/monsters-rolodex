a2d067a590fd1017bc96a5f4da60a76d07eaa31d
Daniel <cdtenzi@gmail.com>
2020-04-13 12:25:57 +0200

    Updates

---
+++ precache-manifest.39301c60111fd3ef637131d3aee51440.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "829a5fa04ac284a4b2c4ce1c913254ce",
+    "url": "/monsters-rolodex/index.html"
+  },
+  {
+    "revision": "2d67ceca98519370b362",
+    "url": "/monsters-rolodex/static/css/main.69f62f30.chunk.css"
+  },
+  {
+    "revision": "926634218f0cd358d7da",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/monsters-rolodex/static/js/2.a6bff23a.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "2d67ceca98519370b362",
+    "url": "/monsters-rolodex/static/js/main.87e3459e.chunk.js"
+  },
+  {
+    "revision": "fc1a59712d7f10d18ab0",
+    "url": "/monsters-rolodex/static/js/runtime-main.664c0a0d.js"
+  }
+]);
